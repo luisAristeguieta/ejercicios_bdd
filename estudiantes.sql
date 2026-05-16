7cb13dd73e1269d13213e502969129721f668d09
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-05-16 16:16:29 -0400

    feat: add repeated students in different courses for testing inserts

--- estudiantes.sql
+++ estudiantes.sql
@@ -21,8 +21,8 @@ values ('1712345670','Luis Enrique','Aristeguieta Jimenez',35, 'Base de Datos','
 (1712345678,'Miguel Angel','Cedeño Salazar',32,'Seguridad Informatica','2026-05-17'),
 (1712345679,'Valeria Nicole','Lopez Macias',20,'Programacion Java','2026-05-18'),
 (1712345680,'Daniel Eduardo','Vera Torres',27,'Base de Datos','2026-05-19'),
-(1712345681,'Camila Isabel','Rojas Mendoza',23,'Redes','2026-05-20'),
-(1712345682,'Fernando Javier','Castro Vera',29,'Desarrollo Web','2026-05-21'),
-(1712345683,'Gabriela Elena','Macias Lopez',25,'Seguridad Informatica','2026-05-22'),
-(1712345684,'Ricardo Jose','Salazar Perez',31,'Programacion Java','2026-05-23');
+(1712345681,'Luis Enrique','Aristeguieta Jimenez',35,'Seguridad Informatica','2026-05-20'),
+(1712345682,'Andrea Sofia','Morales Perez',22,'Redes','2026-05-21'),
+(1712345683,'Carlos Andres','Mendoza Vera',28,'Desarrollo Web','2026-05-22'),
+(1712345684,'Maria Fernanda','Zambrano Lopez',19,'Programacion Java','2026-05-23');
 
